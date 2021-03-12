50524df1bb5e6eaf2f0e1fd985f80583bb2bf965
Andres Cabrera <ancabrera@arcor.com>
2021-03-12 11:47:04 -0300

    Create Project

---
+++ sockets/controller.js
@@ -0,0 +1,24 @@
+
+
+const socketController = (socket) => {
+
+    console.log('Client connected', socket.id)
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected', socket.id);
+    })
+
+    socket.on('send-message', (payload, callback) => {
+
+        const id = 11234;
+        callback(id);
+
+        socket.broadcast.emit('send-message', payload );
+
+    })
+
+}
+
+module.exports = {
+    socketController
+}
